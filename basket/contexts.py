8cb857348dc081a907c62b6f5e2a04b39853d3eb
TMartin88 <tiernanmartin1@gmail.com>
2023-04-22 20:29:36 +0000

    amend csize and colors context and add basket view

--- basket/contexts.py
+++ basket/contexts.py
@@ -5,22 +5,34 @@ from products.models import Product
 
 
 def basket_contents(request):
-
     basket_items = []
     total = 0
     product_count = 0
-
     basket = request.session.get('basket', {})
 
-    for item_id, quantity in basket.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        basket_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+    for item_id, item_data in basket.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            basket_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for color, sizes in item_data['items_by_color'].items():
+                for size, quantity in sizes.items():
+                    total += quantity * product.price
+                    product_count += quantity
+                    basket_items.append({
+                        'item_id': item_id,
+                        'quantity': quantity,
+                        'product': product,
+                        'color': color,
+                        'size': size,
+                    })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
--- basket/views.py
+++ basket/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render, redirect
 
-# Create your views here.
-
 
 def view_basket(request):
     """ A view that renders the basket contents page """
@@ -11,15 +9,31 @@ def view_basket(request):
 
 def add_to_basket(request, item_id):
     """ Add a quantity of the specified product to the shopping basket """
-
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = request.POST.get('product_size', None)
+    color = request.POST.get('product_color', None)
     basket = request.session.get('basket', {})
 
-    if item_id in list(basket.keys()):
-        basket[item_id] += quantity
+    if size or color:
+        item = basket.get(item_id, {'items_by_size': {}, 'items_by_color': {}})
+        item_by_size = item['items_by_size']
+        item_by_color = item['items_by_color']
+        if color in item_by_color and size in item_by_size:
+            item_by_color[color][size] = item_by_color[color].get(size, 0) + quantity
+            item_by_size[size][color] = item_by_size[size].get(color, 0) + quantity
+        elif color in item_by_color:
+            item_by_color[color][size] = quantity
+            item_by_size[size] = {color: quantity}
+        elif size in item_by_size:
+            item_by_size[size][color] = quantity
+            item_by_color[color] = {size: quantity}
+        else:
+            item_by_color[color] = {size: quantity}
+            item_by_size[size] = {color: quantity}
+        basket[item_id] = item
     else:
-        basket[item_id] = quantity
+        basket[item_id] = basket.get(item_id, 0) + quantity
 
     request.session['basket'] = basket
     return redirect(redirect_url)
