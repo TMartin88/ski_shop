6596e38b8e1639c6c8ef2f9ab76d0e23e246c01c
TMartin88 <tiernanmartin1@gmail.com>
2023-04-24 18:51:06 +0000

    remove color functionality from product detail

--- basket/contexts.py
+++ basket/contexts.py
@@ -3,8 +3,8 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
-
 def basket_contents(request):
+
     basket_items = []
     total = 0
     product_count = 0
@@ -22,17 +22,15 @@ def basket_contents(request):
             })
         else:
             product = get_object_or_404(Product, pk=item_id)
-            for color, sizes in item_data['items_by_color'].items():
-                for size, quantity in sizes.items():
-                    total += quantity * product.price
-                    product_count += quantity
-                    basket_items.append({
-                        'item_id': item_id,
-                        'quantity': quantity,
-                        'product': product,
-                        'color': color,
-                        'size': size,
-                    })
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                basket_items.append({
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'product': product,
+                    'size': size,
+                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
@@ -40,9 +38,9 @@ def basket_contents(request):
     else:
         delivery = 0
         free_delivery_delta = 0
-
+    
     grand_total = delivery + total
-
+    
     context = {
         'basket_items': basket_items,
         'total': total,
--- basket/views.py
+++ basket/views.py
@@ -1,39 +1,84 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
+# Create your views here.
 
 def view_basket(request):
     """ A view that renders the basket contents page """
 
     return render(request, 'basket/basket.html')
 
-
 def add_to_basket(request, item_id):
     """ Add a quantity of the specified product to the shopping basket """
+
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    size = request.POST.get('product_size', None)
-    color = request.POST.get('product_color', None)
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     basket = request.session.get('basket', {})
 
-    if size or color:
-        item = basket.get(item_id, {'items_by_size': {}, 'items_by_color': {}})
-        item_by_size = item['items_by_size']
-        item_by_color = item['items_by_color']
-        if color in item_by_color and size in item_by_size:
-            item_by_color[color][size] = item_by_color[color].get(size, 0) + quantity
-            item_by_size[size][color] = item_by_size[size].get(color, 0) + quantity
-        elif color in item_by_color:
-            item_by_color[color][size] = quantity
-            item_by_size[size] = {color: quantity}
-        elif size in item_by_size:
-            item_by_size[size][color] = quantity
-            item_by_color[color] = {size: quantity}
+    if size:
+        if item_id in list(basket.keys()):
+            if size in basket[item_id]['items_by_size'].keys():
+                basket[item_id]['items_by_size'][size] += quantity
+            else:
+                basket[item_id]['items_by_size'][size] = quantity
         else:
-            item_by_color[color] = {size: quantity}
-            item_by_size[size] = {color: quantity}
-        basket[item_id] = item
+            basket[item_id] = {'items_by_size': {size: quantity}}
     else:
-        basket[item_id] = basket.get(item_id, 0) + quantity
+        if item_id in list(basket.keys()):
+            basket[item_id] += quantity
+        else:
+            basket[item_id] = quantity
 
     request.session['basket'] = basket
     return redirect(redirect_url)
+    
+
+def adjust_basket(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    basket = request.session.get('basket', {})
+
+    if size:
+        if quantity > 0:
+            basket[item_id]['items_by_size'][size] = quantity
+        else:
+            del basket[item_id]['items_by_size'][size]
+            if not basket[item_id]['items_by_size']:
+                basket.pop(item_id)
+    else:
+        if quantity > 0:
+            basket[item_id] = quantity
+        else:
+            basket.pop(item_id)
+
+    request.session['basket'] = basket
+    return redirect(reverse('view_basket'))
+
+
+def remove_from_basket(request, item_id):
+    """Remove the item from the shopping basket"""
+
+    try:
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        basket = request.session.get('basket', {})
+
+        if size:
+            del basket[item_id]['items_by_size'][size]
+            if not basket[item_id]['items_by_size']:
+                basket.pop(item_id)
+        else:
+            basket.pop(item_id)
+
+        request.session['basket'] = basket
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
