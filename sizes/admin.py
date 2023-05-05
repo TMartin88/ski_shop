88cb1119a8e677088ccdd168b591ac9ceaaa5a18
TMartin88 <tiernanmartin1@gmail.com>
2023-05-05 10:06:51 +0000

    amend sizes so they are category dependent

--- sizes/admin.py
+++ sizes/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Size, ProductSize
 
+
+class ProductSizeAdmin(admin.ModelAdmin):
+    list_display = ('product', 'size')
+    list_filter = ('product', 'size')
+
+
 admin.site.register(Size)
 admin.site.register(ProductSize)
--- sizes/models.py
+++ sizes/models.py
@@ -4,9 +4,10 @@ from products.models import Product
 
 class Size(models.Model):
     name = models.CharField(max_length=50)
+    category = models.ForeignKey('products.Category', on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.name
+        return f"{self.category} - {self.name}"
 
 
 class ProductSize(models.Model):
