fed7dfe49719abcda18840be149bc07751dceb9c
TMartin88 <tiernanmartin1@gmail.com>
2023-05-05 09:33:34 +0000

    add shipping methods and costs to admin

--- shipping/admin.py
+++ shipping/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import ShippingMethod, ShippingCost
 
-# Register your models here.
+
+@admin.register(ShippingMethod)
+class ShippingMethodAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'active']
+    list_filter = ['active']
+
+
+@admin.register(ShippingCost)
+class ShippingCostAdmin(admin.ModelAdmin):
+    list_display = ['country', 'weight_from', 'weight_to', 'cost', 'method']
+    list_filter = ['country', 'method']
