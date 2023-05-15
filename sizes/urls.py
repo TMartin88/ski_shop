cb9592051981b283fd8cbab17f86ec97f72e5d01
TMartin88 <tiernanmartin1@gmail.com>
2023-05-15 13:42:36 +0000

    migrations

--- sizes/urls.py
+++ sizes/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('add/', views.add_size, name='add_size'),
     path('edit/<int:size_id>/', views.edit_size, name='edit_size'),
     path('delete/<int:size_id>/', views.delete_size, name='delete_size'),
+    path('assign/<int:size_id>/', views.delete_size, name='assign_size'),
 ]
