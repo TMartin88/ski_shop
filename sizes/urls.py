bd3bd2e73ff26c61bd9b8145c7aba31bf986b2cb
TMartin88 <tiernanmartin1@gmail.com>
2023-05-15 13:46:32 +0000

    remove assign sizes from sizes app

--- sizes/urls.py
+++ sizes/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('add/', views.add_size, name='add_size'),
     path('edit/<int:size_id>/', views.edit_size, name='edit_size'),
     path('delete/<int:size_id>/', views.delete_size, name='delete_size'),
-    path('assign/<int:size_id>/', views.delete_size, name='assign_size'),
 ]
