191bdb11023182674d882bb80cf085b2d302453f
TMartin88 <tiernanmartin1@gmail.com>
2023-05-16 11:06:15 +0000

    amend products admin

--- products/admin.py
+++ products/admin.py
@@ -1,10 +1,26 @@
+from django import forms
 from django.contrib import admin
 from .models import Product, Category
 from product_sizes.models import ProductSize
 
 
+class ProductSizeInlineForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        product = self.instance.product if self.instance else None
+        if product:
+            # Filter the sizes based on the selected product's category
+            category = product.category
+            self.fields['size'].queryset = self.fields['size'].queryset.filter(category=category)
+
+    class Meta:
+        model = ProductSize
+        fields = '__all__'
+
+
 class ProductSizeInline(admin.TabularInline):
     model = ProductSize
+    form = ProductSizeInlineForm
 
 
 class ProductAdmin(admin.ModelAdmin):
