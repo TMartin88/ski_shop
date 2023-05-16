be3f51568167e5edd547b9428e084a57bac7c11f
TMartin88 <tiernanmartin1@gmail.com>
2023-05-16 14:31:21 +0000

    amend product admin

--- products/admin.py
+++ products/admin.py
@@ -1,12 +1,21 @@
 from django.contrib import admin
 from .models import Product, Category
 from product_sizes.models import ProductSize
+from sizes.models import Size
+from django.forms import BaseInlineFormSet
+
+
+class ProductSizeFormSet(BaseInlineFormSet):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Filter sizes based on the selected product's category
+        self.queryset = Size.objects.filter(category=self.instance.category)
 
 
 class ProductSizeInline(admin.TabularInline):
     model = ProductSize
     extra = 1
-    can_delete = True
+    formset = ProductSizeFormSet
 
 
 class ProductAdmin(admin.ModelAdmin):
