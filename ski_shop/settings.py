226090c7ea17844413f03c4185ca3713a0adc8c4
TMartin88 <tiernanmartin1@gmail.com>
2023-06-08 14:54:46 +0000

    Fix email send in settings and heroku again

--- ski_shop/settings.py
+++ ski_shop/settings.py
@@ -214,7 +214,7 @@ if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'skishop@example.com'
 else:
-     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
