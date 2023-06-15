b059e6db5138308dd63b3bc0c16ac9b795694199
TMartin88 <tiernanmartin1@gmail.com>
2023-06-15 12:10:05 +0000

    add mailchimp

--- ski_shop/settings.py
+++ ski_shop/settings.py
@@ -26,10 +26,10 @@ if os.path.isfile('env.py'):
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = True
 
-ALLOWED_HOSTS = ['skishop.herokuapp.com', 'localhost', '8000-tmartin88-skishop-u0b1v9chr8e.ws-eu99.gitpod.io']
+ALLOWED_HOSTS = ['skishop.herokuapp.com', 'localhost', '8000-tmartin88-skishop-aupjgx9t1il.ws-eu99.gitpod.io']
 
 # Application definition
 
