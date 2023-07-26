09655d5007e34a3894c0b9f5f9e55c71750797a1
TMartin88 <tiernanmartin1@gmail.com>
2023-07-26 15:43:56 +0000

    add standard delivery calculator to basket

--- basket/contexts.py
+++ basket/contexts.py
@@ -3,6 +3,13 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
+from checkout.models import calculate_shipping_cost
+
+from django.contrib import messages
+
+# Define a custom message level for flat delivery charge
+FLAT_DELIVERY_MESSAGE_LEVEL = 35
+
 
 def basket_contents(request):
 
@@ -11,6 +18,20 @@ def basket_contents(request):
     product_count = 0
     package_weight = 0  # Initialize the package weight
     basket = request.session.get('basket', {})
+
+    # Get the user (if logged in)
+    user = request.user
+
+    # Initialize country code with 'Ireland' as default
+    country_code = 'Ireland'
+
+    # Check if the user is logged in and has a profile with a country code
+    if user.is_authenticated:
+        # Assuming the user's profile model has a 'country' field with the country code
+        # Replace 'Profile' with the actual name of your profile model
+        if hasattr(user, 'userprofile') and hasattr(user.userprofile, 'default_country'):
+            country_code = user.userprofile.default_country.code
+            print(f"User's profile country code: {country_code}")  # Print the country code to the console
 
     for item_id, item_data in basket.items():
         if isinstance(item_data, int):
@@ -36,12 +57,22 @@ def basket_contents(request):
                     'size': size,
                 })
 
-    if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
-        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
-    else:
+    if total == 0:
+        # Empty basket, no delivery charge
         delivery = 0
         free_delivery_delta = 0
+    else:
+        if total < settings.FREE_DELIVERY_THRESHOLD:
+            # delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+            # Calculate the shipping cost based on the package weight and country code
+            delivery, flat_fee_applied = calculate_shipping_cost(package_weight, country_code)
+            free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+            # Check if a flat fee is applied and add a message if so
+            if flat_fee_applied:
+                messages.add_message(request, FLAT_DELIVERY_MESSAGE_LEVEL, "Flat Delivery Charge is applied because we have no normal delivery to your country.")
+        else:
+            delivery = 0
+            free_delivery_delta = 0
 
     grand_total = delivery + total
 
